cdf161aadb0e1dff06342d85ba037e187b80666b
Hide <sharehead@gmail.com>
2017-12-04 21:37:52 +0900

    Version 1.0

---
+++ DCCleaner2/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace DCCleaner2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 203);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setting";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(13, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(111, 25);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '●';
+            this.textBox2.Size = new System.Drawing.Size(111, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "페이지";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PW";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Progress";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(231, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(137, 188);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 219);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(376, 25);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "로그인을 해주세요.";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(69, 20);
+            this.toolStripStatusLabel1.Text = "login plz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 244);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "디시클리너";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ DCCleaner2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinHttp;
+using System.Text.RegularExpressions;
+
+namespace DCCleaner2
+{
+    public partial class Form1 : Form
+    {
+        public WinHttpRequest Winhttp;
+        List<string> pageno = new List<string>();
+        List<string> logno = new List<string>();
+        List<string> board_id = new List<string>();
+        List<string> cid = new List<string>();
+        int time;
+        public static string cookie;
+        public static string user_no; // 넘버
+        public static string ci_t;
+        public static string user_id; // 유저id
+        int page; // 파싱할 페이지번호
+        int board_count = 1;
+        int onoff = 0;
+        public Form1()
+        {
+            Winhttp = new WinHttpRequest();
+            InitializeComponent();
+        }
+
+        public bool login()
+        {
+            Winhttp.Open("POST", "https://dcid.dcinside.com/join/mobile_login_ok.php");
+            Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
+            Winhttp.SetRequestHeader("Referer", "http://m.dcinside.com/login.php?r_url=%2Flist.php%3Fid%3Dhit&rucode=1");
+            Winhttp.Send("user_id="+textBox1.Text+"&user_pw="+textBox2.Text+"&id_chk=on&mode=&id=&r_url=%252Flist.php%253Fid%253Dhit");
+            Winhttp.WaitForResponse();
+
+            Winhttp.Open("GET", "http://m.dcinside.com/list.php?id=hit");
+            Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
+            Winhttp.SetRequestHeader("Referer", "http://m.dcinside.com/login.php?r_url=%2Flist.php%3Fid%3Dhit&rucode=1");
+            Winhttp.Send();
+            Winhttp.WaitForResponse();
+            string data = Winhttp.ResponseText;
+            if (data.IndexOf("logout") != -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+            return DateTime.Now;
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            login();
+            if (login())
+            {
+                button2.Enabled = true;
+                toolStripStatusLabel1.Text = "로그인 성공";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "로그인 실패";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = Convert.ToInt32(textBox3.Text);
+            onoff = 1;
+            if (onoff == 1)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    /* board_id 파싱 */
+                    Winhttp.Open("GET", "http://m.dcinside.com/gallog/list.php?g_id=" + textBox1.Text + "&g_type=G&page=1");
+                    Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                    Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
+                    Winhttp.SetRequestHeader("Referer", "http://m.dcinside.com/gallog/home.php?g_id=" + textBox1.Text);
+                    Winhttp.SetRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    Winhttp.Send();
+                    Winhttp.WaitForResponse();
+                    string datas = Winhttp.ResponseText;
+                    Regex regexs = new Regex("&id=(?<board_id>\\w+)&no=");
+                    MatchCollection mcs = regexs.Matches(datas);
+                    foreach (Match ms in mcs)
+                    {
+                        board_id.Add(ms.Groups["board_id"].Value);
+                        board_count++;
+                        if (board_count > 10)
+                            break;
+                    }
+                    board_count = 1;
+                    /* ci_t 파싱부분 */
+                    Winhttp.Open("GET", "http://gall.dcinside.com/board/view/?id=hit&no=1");
+                    Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                    Winhttp.SetRequestHeader("Referer", "http://gall.dcinside.com/board/view/?id=hit&no=1");
+                    Winhttp.Send();
+                    Winhttp.WaitForResponse();
+                    string data = Winhttp.ResponseText;
+                    string regex = "ci_t\" value=\"(.*?)\" />";
+                    Match match = Regex.Match(data, regex);
+                    if (match.Success)
+                    {
+                        ci_t = match.Groups[1].Value;
+                    }
+                    /* user_id 와 cid 파싱부분 */
+                    Winhttp.Open("GET", "http://gallog.dcinside.com/");
+                    Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                    Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36");
+                    Winhttp.SetRequestHeader("Referer", "http://gallog.dcinside.com/");
+                    Winhttp.SetRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    Winhttp.Send();
+                    Winhttp.WaitForResponse();
+                    string data2 = Winhttp.ResponseText;
+                    Regex regex2 = new Regex("http://gallog.dcinside.com/(?<userid>\\w+)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                    MatchCollection mc = regex2.Matches(data2);
+                    foreach (Match m in mc)
+                    {
+                        user_id = m.Groups["userid"].Value;
+                    }
+
+                    //WinHttpRequest Winhttp = new WinHttpRequest();
+                    Winhttp.Open("GET", "http://gallog.dcinside.com/inc/_mainGallog.php?gid=" + user_id);
+                    Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                    Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36");
+                    Winhttp.SetRequestHeader("Referer", "http://gallog.dcinside.com/inc/_mainGallog.php?gid=" + user_id);
+                    Winhttp.SetRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    Winhttp.Send();
+                    Winhttp.WaitForResponse();
+                    string data3 = Winhttp.ResponseText;
+                    Regex regex3 = new Regex("&cid=(?<cid>\\w+)&", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                    MatchCollection mc2 = regex3.Matches(data3);
+                    foreach (Match m in mc2)
+                    {
+                        cid.Add(m.Groups["cid"].Value);
+                    }
+                    /* pageno 와 logno 파싱 */
+                    Winhttp.Open("GET", "http://gallog.dcinside.com/inc/_mainGallog.php?page=1&gid=" + user_id);
+                    Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                    Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+                    Winhttp.SetRequestHeader("Referer", "http://gallog.dcinside.com/inc/_mainGallog.php?page=1&gid=" + user_id);
+                    Winhttp.SetRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    Winhttp.Send();
+                    Winhttp.WaitForResponse();
+                    string data4 = Winhttp.ResponseText;
+                    Regex regex4 = new Regex("&pno=(?<pageno>\\w+)&logNo=(?<logno>\\w+)&", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                    MatchCollection mc3 = regex4.Matches(data4);
+                    foreach (Match m in mc3)
+                    {
+                        pageno.Add(m.Groups["pageno"].Value);
+                        logno.Add(m.Groups["logno"].Value);
+                    }
+                    if (pageno.Count == 0)
+                    {
+                        MessageBox.Show("글이 없습니다.");
+                        onoff = 0;
+                    }
+                    else
+                    {
+                        Winhttp.Open("GET", "http://m.dcinside.com/gallog/list.php?g_id=" + textBox1.Text + "&g_type=G&page=1");
+                        Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                        Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
+                        Winhttp.SetRequestHeader("Referer", "http://m.dcinside.com/gallog/home.php?g_id=" + textBox1.Text);
+                        Winhttp.SetRequestHeader("X-Requested-With", "XMLHttpRequest");
+                        Winhttp.Send();
+                        Winhttp.WaitForResponse();
+                        /*string data5 = Winhttp.ResponseText;
+                        MessageBox.Show(data5);
+                        Regex regex5 = new Regex("&id=(?<board_id>\\w+)&no=");
+                        MatchCollection mc4 = regex5.Matches(data5);
+                        foreach (Match m in mc4)
+                        {
+                            board_id.Add(m.Groups["board_id"].Value);
+                            count++;
+                            if (count >= 10)
+                                break;
+                        }
+                        count = 1;*/
+
+                        for (int j = 0; j < pageno.Count; j++)
+                        {
+                            /* 갤러리삭제 */
+                            Winhttp.Open("POST", "http://gall.dcinside.com/forms/delete_submit");
+                            Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                            Winhttp.SetRequestHeader("Referer", "http://gall.dcinside.com/");
+                            Winhttp.SetRequestHeader("X-Requested-With", "XMLHttpRequest");
+                            Winhttp.Send("ci_t=" + ci_t + "&id="+board_id[j]+"&no=" + pageno[j] + "&key=");
+                            Delay(200);
+                            /* 갤로그삭제 */
+                            Winhttp.Open("POST", "http://gallog.dcinside.com/inc/_deleteArticle.php");
+                            Winhttp.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                            Winhttp.SetRequestHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
+                            Winhttp.SetRequestHeader("Referer", "http://gallog.dcinside.com");
+                            Winhttp.SetRequestHeader("X-Requested-With", "XMLHttpRequest");
+                            Winhttp.Send("rb=&dTp=1&gid=" + user_id + "&cid=" + cid[j] + "&page=&pno=" + pageno[j] + "&no=" + pageno[j] + "&logNo=" + logno[j] + "&id="+board_id[j]+"&nate=&con_key=aaaaaaaaaaaaaaaaaaaa");
+                            Winhttp.WaitForResponse();
+                            richTextBox1.AppendText(pageno[j] + " -> Del\n");
+                            Delay(200);
+                        }
+                        richTextBox1.AppendText("--Page[" + (page + 1) + "]--\n");
+                    }
+                    pageno.Clear();
+                    logno.Clear();
+                    board_id.Clear();
+                    cid.Clear();
+                }
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(null, null);
+            }
+            if (e.KeyCode == Keys.Tab)
+            {
+                this.Focus();
+                textBox2.SelectAll();
+            }
+        }
+    }
+}
